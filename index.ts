f92687cefaa8ff0f20e4559c23f7cd85b55a7587
Vitalflea <vitalflea@gmail.com>
2025-08-28 00:39:27 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -101,7 +101,8 @@ serve({
       const manifestDecoded = decodeJwt(manifestData);
 
       // Step 4: process pieces
-      const baseUrl: string = catalogDecoded?.config?.remote?.baseUrl;
+      const baseUrlFull: string = catalogDecoded?.config?.remote?.baseUrl || "";
+      const baseUrl = baseUrlFull.split("/direct6/osrs-win/")[0]; // strip suffix
       if (!baseUrl) throw new Error("Base URL not found in catalog config");
       const mergedPieces = await processPieces(manifestDecoded, baseUrl);
 
